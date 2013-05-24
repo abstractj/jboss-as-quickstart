c77777a9129948fdf569be002ac2200c0aa1b87e
Bruno Oliveira <bruno@abstractj.org>
2013-05-24 11:53:19 -0300

    Make use of picketlink schema module instead of provide custom entities

--- picketlink-authorization-idm-jpa/src/main/java/org/jboss/as/quickstarts/picketlink/authorization/idm/jpa/IDMConfiguration.java
+++ picketlink-authorization-idm-jpa/src/main/java/org/jboss/as/quickstarts/picketlink/authorization/idm/jpa/IDMConfiguration.java
@@ -16,33 +16,32 @@
  */
 package org.jboss.as.quickstarts.picketlink.authorization.idm.jpa;
 
+import org.picketlink.idm.config.IdentityConfiguration;
+import org.picketlink.idm.config.IdentityConfigurationBuilder;
+import org.picketlink.idm.jpa.schema.CredentialObject;
+import org.picketlink.idm.jpa.schema.CredentialObjectAttribute;
+import org.picketlink.idm.jpa.schema.IdentityObject;
+import org.picketlink.idm.jpa.schema.IdentityObjectAttribute;
+import org.picketlink.idm.jpa.schema.PartitionObject;
+import org.picketlink.idm.jpa.schema.RelationshipIdentityObject;
+import org.picketlink.idm.jpa.schema.RelationshipObject;
+import org.picketlink.idm.jpa.schema.RelationshipObjectAttribute;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 
-import org.jboss.as.quickstarts.picketlink.authorization.idm.jpa.model.CredentialObject;
-import org.jboss.as.quickstarts.picketlink.authorization.idm.jpa.model.CredentialObjectAttribute;
-import org.jboss.as.quickstarts.picketlink.authorization.idm.jpa.model.IdentityObject;
-import org.jboss.as.quickstarts.picketlink.authorization.idm.jpa.model.IdentityObjectAttribute;
-import org.jboss.as.quickstarts.picketlink.authorization.idm.jpa.model.PartitionObject;
-import org.jboss.as.quickstarts.picketlink.authorization.idm.jpa.model.RelationshipIdentityObject;
-import org.jboss.as.quickstarts.picketlink.authorization.idm.jpa.model.RelationshipObject;
-import org.jboss.as.quickstarts.picketlink.authorization.idm.jpa.model.RelationshipObjectAttribute;
-import org.picketlink.idm.config.IdentityConfiguration;
-import org.picketlink.idm.config.IdentityConfigurationBuilder;
-
 /**
  * This bean produces the configuration for PicketLink IDM
- * 
- * 
- * @author Shane Bryzak
  *
+ * @author Shane Bryzak
  */
 @ApplicationScoped
 public class IDMConfiguration {
 
     private IdentityConfiguration identityConfig = null;
 
-    @Produces IdentityConfiguration createConfig() {
+    @Produces
+    IdentityConfiguration createConfig() {
         if (identityConfig == null) {
             initConfig();
         }
@@ -52,17 +51,17 @@ public class IDMConfiguration {
     private void initConfig() {
         IdentityConfigurationBuilder builder = new IdentityConfigurationBuilder();
         builder
-            .stores()
+                .stores()
                 .jpa()
-                   .identityClass(IdentityObject.class)
-                   .credentialClass(CredentialObject.class)
-                   .credentialAttributeClass(CredentialObjectAttribute.class)
-                   .attributeClass(IdentityObjectAttribute.class)
-                   .relationshipClass(RelationshipObject.class)
-                   .relationshipIdentityClass(RelationshipIdentityObject.class)
-                   .relationshipAttributeClass(RelationshipObjectAttribute.class)
-                   .partitionClass(PartitionObject.class)
-                   .supportAllFeatures();
+                .identityClass(IdentityObject.class)
+                .credentialClass(CredentialObject.class)
+                .credentialAttributeClass(CredentialObjectAttribute.class)
+                .attributeClass(IdentityObjectAttribute.class)
+                .relationshipClass(RelationshipObject.class)
+                .relationshipIdentityClass(RelationshipIdentityObject.class)
+                .relationshipAttributeClass(RelationshipObjectAttribute.class)
+                .partitionClass(PartitionObject.class)
+                .supportAllFeatures();
 
         identityConfig = builder.build();
     }
